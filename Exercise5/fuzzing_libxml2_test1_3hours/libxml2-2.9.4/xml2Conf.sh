be1d3633c2ffa6c113f04f710d436e502723fec4
Yang Jongmun <sheepbelldoor.dev@gmail.com>
2023-02-17 11:48:02 +0900

    fuzzing101 exercise4,5 LibTIFF,LibXML2

---
+++ Exercise5/fuzzing_libxml2_test1_3hours/libxml2-2.9.4/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/jongmunyang/Fuzzing_libxml2/libxml2-2.9.4/install/lib"
+XML2_LIBS="-lxml2 -lz     -lm -ldl"
+XML2_INCLUDEDIR="-I/home/jongmunyang/Fuzzing_libxml2/libxml2-2.9.4/install/include/libxml2"
+MODULE_VERSION="xml2-2.9.4"
+
---
+++ Exercise5/fuzzing_libxml2_test4_increaseInput/libxml2-2.9.4/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/jongmunyang/libxml2_1/install/lib"
+XML2_LIBS="-lxml2 -lz     -lm -ldl"
+XML2_INCLUDEDIR="-I/home/jongmunyang/libxml2_1/install/include/libxml2"
+MODULE_VERSION="xml2-2.9.4"
+
