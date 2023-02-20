94ef5dd96fa4d0f011726eb130b4776e8fdcd3ff
Yang Jongmun <sheepbelldoor.dev@gmail.com>
2023-02-20 15:06:42 +0900

    Add: test 3015a version

---
+++ Exercise5/fuzzing_libxml2_test6_3015a_normal/install_lcov/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/jongmunyang/libxml2_3015a_normal/install/lib"
+XML2_LIBS="-lxml2 -lz     -lm "
+XML2_INCLUDEDIR="-I/home/jongmunyang/libxml2_3015a_normal/install/include/libxml2"
+MODULE_VERSION="xml2-2.9.4"
+
